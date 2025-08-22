1f697a87a1d4e6cacf68b118e8c09077a784f03a
Daemondeal <daemondaeal@gmail.com>
2025-08-22 12:22:03 +0200

    feat: VCD first implementation

--- data/runtime/src/runtime/Runtime.cpp
+++ data/runtime/src/runtime/Runtime.cpp
@@ -1,7 +1,11 @@
 #include "Runtime.hpp"
 #include "ChangeType.hpp"
+#include "Vcd.hpp"
 #include <cassert>
 #include <fmt/format.h>
+#include <fmt/os.h>
+#include <memory>
+#include <string>
 #include <vector>
 
 Circuit::Circuit(int number_of_circuits)
@@ -22,6 +26,19 @@ void Circuit::apply_input(ApplyInputFunc func)
 {
     for (int i = 0; i < m_states.size(); i++) {
         func(&m_states[i], i, m_cycles);
+    }
+}
+
+void Circuit::open_vcd(const std::string path, int circuit_idx) {
+    auto fp = std::make_unique<fmt::ostream>(fmt::output_file(path));
+
+    m_vcd.emplace(std::move(fp), circuit_idx);
+    m_vcd->print_header();
+}
+
+void Circuit::dump_to_vcd() {
+    if (m_vcd.has_value()) {
+        m_vcd->dump(m_states, m_cycles);
     }
 }
 
@@ -87,4 +104,6 @@ void Circuit::eval() {
     m_cycles++;
 
     clone_state(m_states, m_previous_states);
+
+    dump_to_vcd();
 }
