c38ca13cea36191dd3c94af1bd49e54efd985081
Daemondeal <daemondaeal@gmail.com>
2025-09-18 13:14:27 +0200

    cleanup: multiplier main.cu

--- demos/multiplier/main.cu
+++ demos/multiplier/main.cu
@@ -3,33 +3,16 @@
 #include "codegen/module.hpp"
 #include "runtime/Runtime.hpp"
 #include "runtime/UserProvided.hpp"
-
-struct xorshift32_state {
-    uint32_t a;
-};
-
-/* The state must be initialized to non-zero */
-__device__ uint32_t xorshift32(xorshift32_state *state)
-{
-    /* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
-    uint32_t x = state->a;
-    x ^= x << 13;
-    x ^= x >> 17;
-    x ^= x << 5;
-    return state->a = x;
-}
+#include "runtime/Utils.hpp"
 
 
 __global__ void cudalator_apply_input(StateType *dut, int cycle, size_t len) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid >= len) return;
 
-    int seed = cycle ^ tid;
-    xorshift32_state rng;
-    rng.a = seed;
-    xorshift32(&rng);
-    uint32_t a = xorshift32(&rng);
-    uint32_t b = xorshift32(&rng);
+    cu::Random random(cycle ^ tid);
+    uint32_t a = random.get();
+    uint32_t b = random.get();
 
     dut[tid].i_A = Bit<8>(a % 256);
     dut[tid].i_B = Bit<8>(b % 256);
@@ -44,26 +27,11 @@ int main(int argc, char *argv[]) {
     Circuit circuit(circuit_number);
 
     constexpr int MAX = 1000;
-    constexpr int STEP = std::max(MAX / 1000, 1);
 
     // circuit.open_vcd("waves.vcd", 3);
     fmt::println("Starting Simulation");
     for (int i = 0; i < MAX; i++) {
-        if (i % STEP == 0) {
-            int steps = (20 * (i+1)) / MAX;
-
-            fmt::print("\r[");
-            for (int j = 0; j < steps; j++) {
-                fmt::print("#");
-            }
-
-            for (int j = steps; j < 20; j++) {
-                fmt::print(".");
-            }
-            fmt::print("] {}/{}", i+1, MAX);
-            fflush(stdout);
-        }
-
+        cu::progress_display(i, MAX);
 
         circuit.apply_input();
         circuit.eval();
